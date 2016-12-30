cf30ab356ca3ade9255ed63693c7876cf6a97377
Will Leonardi <wleonardi@riavera.com>
2016-12-30 13:03:16 -0500

    Added polymorphic many-to-many relationships.

--- src/Propel/Generator/Builder/Om/ObjectBuilder.php
+++ src/Propel/Generator/Builder/Om/ObjectBuilder.php
@@ -4243,10 +4243,16 @@ abstract class ".$this->getUnqualifiedClassName().$parentClass." implements Acti
             foreach ($crossFKs->getIncomingForeignKey()->getColumnObjectsMapping() as $reference) {
                 $local   = $reference['local'];
                 $foreign = $reference['foreign'];
+                $value   = $reference['value'];
 
                 $idx = array_search($local, $crossPks, true);
-                $script .= "
+                if ($value) {
+                    $script .= "
+                        \$entryPk[$idx] = '$value';";
+                } else {
+                    $script .= "
                         \$entryPk[$idx] = \$this->get{$foreign->getPhpName()}();";
+                }
             }
 
             $combinationIdx = 0;
@@ -4254,10 +4260,16 @@ abstract class ".$this->getUnqualifiedClassName().$parentClass." implements Acti
                 foreach ($crossFK->getColumnObjectsMapping() as $reference) {
                     $local   = $reference['local'];
                     $foreign = $reference['foreign'];
+                    $value   = $reference['value'];
 
                     $idx = array_search($local, $crossPks, true);
-                    $script .= "
-                        \$entryPk[$idx] = \$combination[$combinationIdx]->get{$foreign->getPhpName()}();";
+                    if ($value) {
+                        $script .= "
+                            \$entryPk[$idx] = '$value';";
+                    } else {
+                        $script .= "
+                            \$entryPk[$idx] = \$combination[$combinationIdx]->get{$foreign->getPhpName()}();";
+                    }
                 }
                 $combinationIdx++;
             }
@@ -4291,20 +4303,32 @@ abstract class ".$this->getUnqualifiedClassName().$parentClass." implements Acti
             foreach ($crossFKs->getIncomingForeignKey()->getColumnObjectsMapping() as $reference) {
                 $local   = $reference['local'];
                 $foreign = $reference['foreign'];
+				$value   = $reference['value'];
 
                 $idx = array_search($local, $crossPks, true);
-                $script .= "
-                        \$entryPk[$idx] = \$this->get{$foreign->getPhpName()}();";
+				if ($value) {
+                    $script .= "
+                		\$entryPk[$idx] = '$value';";
+				} else {
+					$script .= "
+						\$entryPk[$idx] = \$this->get{$foreign->getPhpName()}();";
+				}
             }
 
             $crossFK = $crossFKs->getCrossForeignKeys()[0];
             foreach ($crossFK->getColumnObjectsMapping() as $reference) {
                 $local   = $reference['local'];
                 $foreign = $reference['foreign'];
+				$value   = $reference['value'];
 
                 $idx = array_search($local, $crossPks, true);
-                $script .= "
+				if ($value) {
+                    $script .= "
+                		\$entryPk[$idx] = '$value';";
+				} else {
+					$script .= "
                         \$entryPk[$idx] = \$entry->get{$foreign->getPhpName()}();";
+				}
             }
 
             $script .= "
